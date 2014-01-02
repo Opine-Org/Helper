fb63a075ddd447fca91b961e6833728928d159af
webuser <ryan@virtuecenter.com>
2014-01-02 02:37:32 -0500

    finished blurb helper

---
+++ available/Blurb.php
@@ -0,0 +1,16 @@
+<?php
+return function ($template, $context, $args, $source) {
+    $db = Framework\container()->db;
+    $args = $template->htmlArgsToArray($args);
+    if (isset($args['title'])) {
+    	$blurb = $db->collection('blurbs')->findOne(['title' => $args['title']]);
+    } elseif (isset($args['tag'])) {
+    	$blurb = $db->collection('blurbs')->findOne(['tags' => $args['tag']]);
+    } else {
+    	return '';
+    }
+    if (isset($blurb['_id'])) {
+    	return $blurb['body'];
+    }
+    return '';
+};
