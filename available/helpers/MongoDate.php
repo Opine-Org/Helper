d23bfec1dcc9af86c0981c025b2a8e8637ec83fa
webuser <ryan@virtuecenter.com>
2014-10-22 13:31:39 -0400

    bug fix

--- available/helpers/MongoDate.php
+++ available/helpers/MongoDate.php
@@ -1,13 +1,20 @@
 <?php
 return function ($args, $options=[]) {
 	//usage: {{MongoDate field format="m/d/Y"}}
-	$dateObject = $args[0];
+    $dateObject = $args[0];
     $format = 'm/d/Y';
     if (isset($options['format'])) {
         $format = $options['format'];
     }
-    if (!is_object($dateObject)) {
-        return date($format, strtotime($dateObject));
+    if (is_object($dateObject)) {
+        return date($format, $dateObject->sec);
     }
-    return date($format, $dateObject->sec);
+    if (is_array($dateObject)) {
+        if (isset($dateObject['sec'])) {
+            return date($format, $dateObject['sec']);
+        } else {
+            return '[Array]';
+        }
+    }
+    return date($format, strtotime($dateObject));
 };
