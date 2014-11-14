520f2aea83f56d4bdabf1cf55fe528ef6c974b7b
Ryan Mahoney <ryan@virtuecenter.com>
2014-11-14 08:55:20 -0500

    test

---
+++ available/helpers/ContentPartial.php
@@ -0,0 +1,15 @@
+<?php
+return function ($arguments, $options) {
+    $db = \Opine\container()->db;
+    if (isset($options['title'])) {
+        $blurb = $db->collection('partials')->findOne(['title' => $options['title']]);
+    } elseif (isset($options['code'])) {
+        $blurb = $db->collection('partials')->findOne(['code_name' => $options['code']]);
+    } else {
+        return '';
+    }
+    if (isset($blurb['_id'])) {
+        return $blurb['body'];
+    }
+    return '';
+};
